5223dce01c5b1251beabc2d74ab3e99930e43c1f
ondsmid <ondsmid@seznam.cz>
2023-08-20 01:39:51 +0200

    shortcuts hint

--- _show_shortcuts_of_VisualStudio.ps1
+++ _show_shortcuts_of_VisualStudio.ps1
@@ -1,10 +1,15 @@
 echo @'
-View.NavigateBackward: Alt+l
-View.NavigateForward: Alt+;
+Edit.CharLeft: Alt+J
+Edit.CharRight: Alt+L
+Edit.LineUp: Alt+I
+Edit.LineDown: Alt+K
 
-Edit.WordPrevious: Alt+J
-Edit.WordNext: Alt+K
+Edit.WordPrevious: Alt+U
+Edit.WordNext: Alt+O
 
 Edit.WordPreviousExtend: shift+Alt+J
 Edit.WordNextExtend: shift+Alt+K
+
+View.NavigateBackward: Alt+l
+View.NavigateForward: Alt+;
 '@
